300b667a1baf40c26accaa573fd88c2151efbf84
Surbma <peter@surbma.hu>
2013-01-16 12:28:58 +0100

    130116
    - Updated Bootstrap to 2.2.2
    - Updated FontAwesome to 3.0.1
    - fixed some style regarding the new updates

--- surbma-twitter-bootstrap.php
+++ surbma-twitter-bootstrap.php
@@ -4,7 +4,7 @@
 Plugin Name: Surbma - Twitter Bootstrap
 Plugin URI: http://surbma.com/
 Description: Twitter Bootstrap styles
-Version: 120928
+Version: 130116
 Author: Surbma
 Author URI: http://surbma.com/
 License: GPL2
@@ -23,8 +23,8 @@ function surbma_tb_scripts() {
 add_action( 'wp_footer', 'surbma_tb_scripts' );
 
 function surbma_tb_enqueue_scripts() {
-	wp_enqueue_script( 'bootstrap-scripts', plugins_url( '', __FILE__ ) . '/js/bootstrap.min.js', array('jquery'), '2.1.1', true );
-  wp_enqueue_style( 'bootstrap-styles', plugins_url( '', __FILE__ ) . '/css/bootstrap.min.css', false, '2.1.1' );
+	wp_enqueue_script( 'bootstrap-scripts', plugins_url( '', __FILE__ ) . '/js/bootstrap.min.js', array('jquery'), '2.2.2', true );
+  wp_enqueue_style( 'bootstrap-styles', plugins_url( '', __FILE__ ) . '/css/bootstrap.min.css', false, '2.2.2' );
 }
 add_action( 'wp_enqueue_scripts', 'surbma_tb_enqueue_scripts' );
 
