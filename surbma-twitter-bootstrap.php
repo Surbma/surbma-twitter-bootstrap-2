befedf8b23505de6702b5d16528e86219b6ac4e4
Surbma <peter@surbma.hu>
2013-01-22 16:19:32 +0100

    130122
    Added new tags to $allowedposttags to allow users to use this plugin
    without the unfiltered_html capability.

--- surbma-twitter-bootstrap.php
+++ surbma-twitter-bootstrap.php
@@ -4,7 +4,7 @@
 Plugin Name: Surbma - Twitter Bootstrap
 Plugin URI: http://surbma.com/
 Description: Twitter Bootstrap styles
-Version: 130118
+Version: 130122
 Author: Surbma
 Author URI: http://surbma.com/
 License: GPL2
@@ -28,3 +28,20 @@ function surbma_tb_enqueue_scripts() {
 }
 add_action( 'wp_enqueue_scripts', 'surbma_tb_enqueue_scripts' );
 
+global $allowedposttags;
+$allowedposttags['a'] = array(
+  'class' => array(),
+  'id' => array(),
+  'style' => array(),
+  'title' => array(),
+  'href' => array(),
+  'rel' => array(),
+  'rev' => array(),
+  'name' => array(),
+  'target' => array(),
+  'data-toggle' => array(),
+  'data-target' => array(),
+  'data-content' => array(),
+  'data-parent' => array(),
+  'data-dismiss' => array()
+);
