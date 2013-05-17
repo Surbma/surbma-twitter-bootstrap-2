3cc670fe21d4e880b6ad145cc370b157c25005aa
Surbma <peter@surbma.hu>
2013-05-17 22:40:48 +0200

    1.2.0
    New versioning
    Twitter Bootstrap upgraded to 2.3.1
    Font Awesome ugraded to 3.1.0

--- surbma-twitter-bootstrap.php
+++ surbma-twitter-bootstrap.php
@@ -4,13 +4,13 @@
 Plugin Name: Surbma - Twitter Bootstrap
 Plugin URI: http://surbma.com/
 Description: Twitter Bootstrap styles
-Version: 130122
+Version: 1.2.0
 Author: Surbma
 Author URI: http://surbma.com/
 License: GPL2
 */
 
-function surbma_tb_scripts() {
+function surbma_twitter_bootstrap_scripts() {
 	?><script type="text/javascript">
   	!function ($) {
   		$(function() {
@@ -20,13 +20,13 @@ function surbma_tb_scripts() {
   	}(window.jQuery)
   </script><?php
 }
-add_action( 'wp_footer', 'surbma_tb_scripts' );
+add_action( 'wp_footer', 'surbma_twitter_bootstrap_scripts' );
 
-function surbma_tb_enqueue_scripts() {
-	wp_enqueue_script( 'bootstrap-scripts', plugins_url( '', __FILE__ ) . '/js/bootstrap.min.js', array('jquery'), '2.2.2', true );
-  wp_enqueue_style( 'bootstrap-styles', plugins_url( '', __FILE__ ) . '/css/bootstrap.min.css', false, '2.2.2' );
+function surbma_twitter_bootstrap_enqueue_scripts() {
+	wp_enqueue_script( 'bootstrap-scripts', plugins_url( '', __FILE__ ) . '/js/bootstrap.min.js', array('jquery'), '2.3.1', true );
+  wp_enqueue_style( 'bootstrap-styles', plugins_url( '', __FILE__ ) . '/css/bootstrap.min.css', false, '2.3.1' );
 }
-add_action( 'wp_enqueue_scripts', 'surbma_tb_enqueue_scripts' );
+add_action( 'wp_enqueue_scripts', 'surbma_twitter_bootstrap_enqueue_scripts' );
 
 global $allowedposttags;
 $allowedposttags['a'] = array(
