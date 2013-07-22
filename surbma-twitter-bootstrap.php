acced35c07088404e5e5de76ffe7d80368a04609
Surbma <peter@surbma.hu>
2013-07-22 16:08:24 +0200

    1.2.1 - Added more allowedposttags
    - Added more allowedposttags
    - Spaces changed to tabs

--- surbma-twitter-bootstrap.php
+++ surbma-twitter-bootstrap.php
@@ -4,44 +4,52 @@
 Plugin Name: Surbma - Twitter Bootstrap
 Plugin URI: http://surbma.com/
 Description: Twitter Bootstrap styles
-Version: 1.2.0
+Version: 1.2.1
 Author: Surbma
 Author URI: http://surbma.com/
 License: GPL2
 */
 
 function surbma_twitter_bootstrap_scripts() {
-	?><script type="text/javascript">
-  	!function ($) {
-  		$(function() {
-  			$(".tip").tooltip()
-  			$(".pop").popover()
-  		})
-  	}(window.jQuery)
-  </script><?php
+?><script type="text/javascript">
+	!function ($) {
+		$(function() {
+			$(".tip").tooltip()
+			$(".pop").popover()
+		})
+	}(window.jQuery)
+</script><?php
 }
 add_action( 'wp_footer', 'surbma_twitter_bootstrap_scripts' );
 
 function surbma_twitter_bootstrap_enqueue_scripts() {
-	wp_enqueue_script( 'bootstrap-scripts', plugins_url( '', __FILE__ ) . '/js/bootstrap.min.js', array('jquery'), '2.3.1', true );
-  wp_enqueue_style( 'bootstrap-styles', plugins_url( '', __FILE__ ) . '/css/bootstrap.min.css', false, '2.3.1' );
+	wp_enqueue_script( 'bootstrap-scripts', plugins_url( '', __FILE__ ) . '/js/bootstrap.min.js', array( 'jquery' ), '2.3.1', true );
+	wp_enqueue_style( 'bootstrap-styles', plugins_url( '', __FILE__ ) . '/css/bootstrap.min.css', false, '2.3.1' );
 }
 add_action( 'wp_enqueue_scripts', 'surbma_twitter_bootstrap_enqueue_scripts' );
 
 global $allowedposttags;
 $allowedposttags['a'] = array(
-  'class' => array(),
-  'id' => array(),
-  'style' => array(),
-  'title' => array(),
-  'href' => array(),
-  'rel' => array(),
-  'rev' => array(),
-  'name' => array(),
-  'target' => array(),
-  'data-toggle' => array(),
-  'data-target' => array(),
-  'data-content' => array(),
-  'data-parent' => array(),
-  'data-dismiss' => array()
+	'class' => array(),
+	'id' => array(),
+	'style' => array(),
+	'title' => array(),
+	'href' => array(),
+	'rel' => array(),
+	'rev' => array(),
+	'name' => array(),
+	'target' => array(),
+	'data-animation' => array(),
+	'data-html' => array(),
+	'data-placement' => array(),
+	'data-selector' => array(),
+	'data-trigger' => array(),
+	'data-title' => array(),
+	'data-delay' => array(),
+	'data-container' => array(),
+	'data-toggle' => array(),
+	'data-target' => array(),
+	'data-content' => array(),
+	'data-parent' => array(),
+	'data-dismiss' => array()
 );
